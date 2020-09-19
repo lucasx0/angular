ed7736918349cefbde4a016df7dabffd4302a898
lucas.almeida <lucas.almeida@usto.re>
2020-09-19 11:35:33 -0300

    diretivas estrutural e de atributos

--- frontend/src/app/directives/for.directive.ts
+++ frontend/src/app/directives/for.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, OnInit, Input, ViewContainerRef, TemplateRef } from '@angular/core';
+import { Template } from '@angular/compiler/src/render3/r3_ast';
 
 @Directive({
   selector: '[myFor]'
 })
-export class ForDirective {
+export class ForDirective implements OnInit {
 
-  constructor() { }
+  @Input('myForEm') numbers: number[];
+
+
+  constructor(
+    private container: ViewContainerRef, 
+    private template: TemplateRef<any>) {
+    
+   }
+
+   ngOnInit(): void {
+    for(let number of this.numbers){
+      this.container.createEmbeddedView(
+        this.template, {$implicit: number});
+    }
+    /* console.log(this.numbers); */
+   }
 
 }

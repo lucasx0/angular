955c963acde48118af88e8764ab52d12489b77f9
lucas.almeida <lucas.almeida@usto.re>
2020-09-26 10:59:22 -0300

    criando metodo e componente de produto e exibição

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -24,7 +24,8 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { FormsModule } from '@angular/forms'
 import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
+import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component'
 
 
 
@@ -39,7 +40,8 @@ import { MatInputModule } from '@angular/material/input'
     ProductCrudComponent,
     RoxoDirective,
     ForDirective,
-    ProductCreatComponent
+    ProductCreatComponent,
+    ProductReadComponent
     
   ],
   imports: [

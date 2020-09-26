b46fc48de2678151d28eec434adce1c52766e78e
lucas.almeida <lucas.almeida@usto.re>
2020-09-26 09:55:15 -0300

    criando formulario para obter produtos. E fiz uma alteração na cor do input.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -20,8 +20,11 @@ import { ForDirective } from './directives/for.directive';
 import { ProductCreatComponent } from './components/product/product-creat/product-creat.component';
 import { MatButtonModule } from '@angular/material/button' 
 import { MatSnackBarModule } from '@angular/material/snack-bar' 
-
 import { HttpClientModule } from '@angular/common/http'
+
+import { FormsModule } from '@angular/forms'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
 
 
 
@@ -49,7 +52,11 @@ import { HttpClientModule } from '@angular/common/http'
     MatCardModule,
     MatButtonModule,
     MatSnackBarModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
+
     
   ],
   providers: [],
